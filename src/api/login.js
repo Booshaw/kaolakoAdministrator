4721201c9aa7e0f8e260dd5c546398c9140b2fc9
booshaw <booshaw@booshaw.local>
2018-03-07 15:19:25 +0800

    change /user/login

--- src/api/login.js
+++ src/api/login.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 const debug = process.env.NODE_ENV !== 'production'
 export function sendLogin(params) {
-  const url = debug ? '/login' : 'http://192.168.0.6:9080/jiahuan/user/login'
+  const url = debug ? '/login' : 'http://192.168.0.6:9080/jiahuan/login'
   return axios.post(url, params).then((res) => {
     return Promise.resolve(res)
   })
