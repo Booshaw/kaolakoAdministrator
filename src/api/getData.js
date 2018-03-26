51d20f6184bf8a4758b3b6908fecccaf2dbe73ce
booshaw <booshaw@booshaw.local>
2018-03-26 10:56:11 +0800

    增加本地api路径常量

--- src/api/getData.js
+++ src/api/getData.js
@@ -1,65 +1,66 @@
 import axios from 'axios'
 const debug = process.env.NODE_ENV !== 'production'
+const serverUrl = 'http://192.168.0.6:9080/jiahuan/'
 
 // 获取数据字典
 export function getDict(params) {
-  const url = debug ? '/common/dict/get' : 'http://192.168.0.6:9080/jiahuan/common/dict/get'
+  const url = debug ? '/common/dict/get' : `${serverUrl}common/dict/get`
   return axios.post(url, params).then((res) => {
     return Promise.resolve(res.data)
   })
 }
 // 获取患者列表
 export function getPatientList(params) {
-  const url = debug ? '/patient/list' : 'http://192.168.0.6:9080/jiahuan/patient/list'
+  const url = debug ? '/patient/list' : `${serverUrl}patient/list`
   return axios.post(url, params).then((res) => {
     return Promise.resolve(res.data)
   })
 }
 // 查询患者个人信息
 export function getPatientInfo(params) {
-  const url = debug ? '/patient/get' : 'http://192.168.0.6:9080/jiahuan/patient/get'
+  const url = debug ? '/patient/get' : `${serverUrl}patient/get`
   return axios.post(url, params).then((res) => {
     return Promise.resolve(res.data)
   })
 }
 // 获取疾病分类
 export function getDiseaseCategory(params) {
-  const url = debug ? '/common/checkup/category' : 'http://192.168.0.6:9080/jiahuan/common/checkup/category'
+  const url = debug ? '/common/checkup/category' : `${serverUrl}common/checkup/category`
   return axios.post(url, params).then((res) => {
     return Promise.resolve(res.data)
   })
 }
 // 获取疾病检查数据字段
 export function getDiseaseList(params) {
-  const url = debug ? '/common/checkup/list' : 'http://192.168.0.6:9080/jiahuan/common/checkup/list'
+  const url = debug ? '/common/checkup/list' : `${serverUrl}common/checkup/list`
   return axios.post(url, params).then((res) => {
     return Promise.resolve(res.data)
   })
 }
 // 获取医生个人信息
 export function getDoctorInformation(params) {
-  const url = debug ? '/doctor/detail/get' : 'http://192.168.0.6:9080/jiahuan/doctor/detail/get'
+  const url = debug ? '/doctor/detail/get' : `${serverUrl}doctor/detail/get`
   return axios.post(url, params).then((res) => {
     return Promise.resolve(res.data)
   })
 }
 // 获取医疗团队详情
 export function getDoctorGroupInformation(params) {
-  const url = debug ? '/doctor/studio/get' : 'http://192.168.0.6:9080/jiahuan/doctor/studio/get'
+  const url = debug ? '/doctor/studio/get' : `${serverUrl}doctor/studio/get`
   return axios.post(url, params).then((res) => {
     return Promise.resolve(res.data)
   })
 }
 // 获取医疗团队会员列表
 export function getMemberList(params) {
-  const url = debug ? '/doctor/vip/member/list' : 'http://192.168.0.6:9080/jiahuan/doctor/vip/member/list'
+  const url = debug ? '/doctor/vip/member/list' : `${serverUrl}doctor/vip/member/list`
   return axios.post(url, params).then((res) => {
     return Promise.resolve(res.data)
   })
 }
 // 获取医疗团队随访列表
 export function getFollowList(params) {
-  const url = debug ? '/doctor/follow/list' : 'http://192.168.0.6:9080/jiahuan/doctor/follow/list'
+  const url = debug ? '/doctor/follow/list' : `${serverUrl}doctor/follow/list`
   return axios.post(url, params).then((res) => {
     return Promise.resolve(res.data)
   })
