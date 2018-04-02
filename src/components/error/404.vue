92014b64b4155b5f8cd12aeb88c9688deba4b830
booshaw <booshaw@booshaw.local>
2018-04-02 15:16:51 +0800

    404页面

---
+++ src/components/error/404.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="error-wrappr">
+    <div class="back-wrapper">
+      <img src="./404.jpeg" alt="" @click.stop="toHome">
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {}
+  },
+  methods: {
+    toHome() {
+      this.$router.push({
+        path: '/'
+      })
+    }
+  }
+}
+</script>
+<style lang="stylus">
+.error-wrapper
+  width 100%
+  background-color #fffcf5
+  background-size cover
+  .back-wrapper
+    width 20%
+    margin 0 auto
+    top 45%
+</style>
