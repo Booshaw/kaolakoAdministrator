a48ea9cbd0107d103b6eb04e18af9f74b54fafa4
booshaw <booshaw@booshaw.local>
2018-05-07 12:35:32 +0800

    login api

--- src/api/login.js
+++ src/api/login.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 const debug = process.env.NODE_ENV !== 'production'
 export function sendLogin(params) {
-  const url = debug ? '/login' : ''
+  const url = debug ? '/login' : 'http://kaola.eaon.win:8080/kaola/login'
   return axios.post(url, params).then((res) => {
     return Promise.resolve(res)
   })
